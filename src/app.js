8a0e5f78c1158a961afbd8d9c061717e4e26f6de
Mohit Pratap Mehra <52151831+pratapm537@users.noreply.github.com>
2025-12-29 13:40:26 +0530

    created all important directories

---
+++ src/app.js
@@ -0,0 +1,33 @@
+import express from "express";
+
+import cors from 'cors'
+
+const app = express();
+
+//basic configration
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended:true, limit: "16kb"}))
+app.use(express.static("public"))
+
+//cors configration
+app.use(cors({
+    origin: process.env.CROS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET","POST","PUT","PATCH","OPTION","DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}))
+
+app.get('/',(req,res) => {
+    res.send("<h1>Welcome to -Home</h1>")
+})
+
+app.get('/login',(req,res) => {
+    res.send("<h1>you are logged now</h1>")
+})
+
+app.get('/course',(req,res) => {
+    res.send("<h1>Welcome to -Course</h1>")
+})
+
+
+export default app;
