10465377ddb09aa9752a94650d0984ba3bd3a773
Mohit Pratap Mehra <52151831+pratapm537@users.noreply.github.com>
2025-12-30 18:56:22 +0530

    add mongoose and mongodb

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import dotenv from "dotenv";
 import express from "express"
 import app from "./app.js";
+import connectDB from "./db/index.js"
 
 dotenv.config({ path: './.env' })
 
 const port = process.env.PORT || 3000
 
 
-app.listen(port, () => {
+connectDB()
+.then(
+    app.listen(port, () => {
     console.log(`server is running on port http://localhost:${port}`)
+})
+)
+.catch((err) => {
+    console.lor("connection error ", err)
 })
 
 
---
+++ src/utils/constants.js
@@ -0,0 +1,13 @@
+export const UserRoleEnum = {
+    ADMIN: "admin",
+    PROJECT_ADMIN: "project_admin",
+    MEMBER: "member"
+}
+
+export const AvailableUserRoleEnum = Object.values(UserRoleEnum)
+
+export const TaskStatusEnum = {
+    TODO: "todo",
+    IN_PROGRESS: "in_progress",
+    DONE: "done"
+}
